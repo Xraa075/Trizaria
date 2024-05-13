b71c697d15098dfaa2d7fb27da3de2ca44278d4e
rijuldnt <rizalldnt@gmail.com>
2024-05-13 15:57:37 +0700

    first cuy

---
+++ src/dashboard_kasir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <title>dashboard_kasir</title>
+</head>
+
+<body class="bg-black-100">
+    <h1>tes</h1>
+</body>
+
+</html>
---
+++ src/loginkasir.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'connection.php';
+
+if(isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM kasir WHERE username_kasir = '$username'");
+
+    if(mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row["password_kasir"])) {
+            header("Location: dashboard_kasir.php");
+            exit;
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username: </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password: </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <button type="submit" name="login">Login</button>
+        </ul>
+    </form>
+
+</body>
+
+</html>
