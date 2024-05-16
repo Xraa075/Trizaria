a2a993917242e815d8a51fd81d936bba0736b740
Xraa075 <phone.poco123@gmail.com>
2024-05-17 06:28:01 +0700

    haduh ternyata

---
+++ src/dashboard_kasir.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <link rel="stylesheet" href="dashboard_kasir.css">
+    <title>DASHBOARD KASIR</title>
+</head>
+
+<body>
+    <div class="container">
+        <img src="asset/logotrizaria.svg">
+        <h2>Slice into happiness your perfect pizza experience</h2>
+        <form>
+            <button>New Order</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ src/loginkasir.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'connection.php';
+
+if(isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM kasir WHERE username_kasir = '$username'");
+
+    if(mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row["password_kasir"])) {
+            header("Location: dashboard_kasir.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginkasir.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <img src="asset/logotrizaria.svg" class="img">
+        <h1>LOGIN</h1>
+
+        <?php if (isset($error)) : ?>
+        <p>username / password salah!!</p>
+        <?php endif; ?>
+
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <input type="text" name="username" placeholder="username">
+                </li>
+                <li>
+                    <input type="password" name="password" placeholder="password">
+                </li>
+                <button type="submit" name="login" class="submit">Login</button>
+            </ul>
+        </form>
+    </div>
+</body>
+
+</html>''
