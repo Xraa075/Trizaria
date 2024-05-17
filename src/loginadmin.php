ccbcc9dfbfcbb6f34806a18aaa5abe0564479f64
rijuldnt <rizalldnt@gmail.com>
2024-05-17 13:20:22 +0700

    memperbaiki input

--- src/loginadmin.php
+++ src/loginadmin.php
@@ -26,7 +26,7 @@ if(isset($_POST["login"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="loginkasir.css">
+    <link rel="stylesheet" href="login_admin.css">
     <title>Login</title>
 </head>
 
