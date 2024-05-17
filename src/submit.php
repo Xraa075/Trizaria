f609811658662065cd9840d5e3b85647725afe76
rijuldnt <rizalldnt@gmail.com>
2024-05-17 22:29:16 +0700

    menambahkan beberapa halaman di bagian admin

---
+++ src/submit.php
@@ -0,0 +1,21 @@
+<?php
+include "koneksi.php" ;
+
+$gambar = $_FILES['gambar']['name'];
+	$file_tmp = $_FILES['gambar']['tmp_name'] ;
+	$nama = $_POST['nama'];
+    $detail = $_POST['detail'];
+    $harga = $_POST['harga'];
+    $stok = $_POST['stok'];
+    move_uploaded_file($file_tmp, 'file/'.$gambar) ;
+    $query = "INSERT INTO makanan SET 
+								    gambar = '$gambar',
+								    nama = '$nama',
+                                    detail = '$detail',
+                                    harga = '$harga',
+                                    stok = '$stok'
+	";
+    mysqli_query($koneksi, $query) 
+	or die("SQL Error " .mysqli_error());
+    header('location:manajemen.php');
+?>
